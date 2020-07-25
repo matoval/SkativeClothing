73aa49936d4cb790a4be7b06f3dbf93b7f1d98d8
Matthew Sandoval <mat24c@gmail.com>
2020-07-25 12:19:13 -0700

    added about and cart pages

---
+++ src/pages/cart.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const CartPage = () => (
+  <Layout>
+    <SEO title="Cart" /> 
+    <div
+      className="cart"
+    >
+    </div>
+  </Layout>
+)
+
+export default CartPage
